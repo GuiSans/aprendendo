da2f6a28379ac7571e6d7c55b778536e2b378ce8
GuiSans <148245986+GuiSans@users.noreply.github.com>
2025-09-16 21:21:36 -0300

    mudanças: apenas melhorei a interface principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Atv1Apresentacao.dart';
 
 void main() {
   runApp(MeuApp());
@@ -10,14 +11,80 @@ class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: SafeArea(child: Center(
-        child: Column(
-          children: <Widget>[
-            Text('''Olá mundo! :D
-            Esse é um aplicativo que na verdade é uma compilação das minhas atividades do curso do polimorfismo! São 90 aulas e o professor é muito bom!''', ),
-          ],
+      home: Scaffold(
+        backgroundColor: Color(0xFFFEDCBA),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Title(
+                      color: Color(0xFF001B24),
+                      child: Text(
+                        'Olá Mundo!',
+                        style: TextStyle(
+                          fontFamily: 'Pacifico',
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 7),
+                    Title(
+                      color: Colors.black,
+                      child: Text(
+                        ':D',
+                        style: TextStyle(
+                          fontSize: 60.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 250,
+                  child: Divider(color: Colors.deepOrange.shade100),
+                ),
+                Text(
+                  '''Esse é um aplicativo que na verdade é uma compilação das minhas atividades do curso do polimorfismo! São 90 aulas e o professor é muito bom!''',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
         ),
-      ))),
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xFFFEDCBA),
+          child: SizedBox(
+            height: 60,
+            child: Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 201, 141, 81),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+                  textStyle:
+                      TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Apresentacao(),
+                    ),
+                  );
+                },
+                child: (Text('Próxima atividade', style: TextStyle(color: Colors.black),)),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
